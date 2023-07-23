ca9908fa3668ba0f0e5563e37c30aa0d223f5cd7
Oussamaghribi <ghribioussama98@gmail.com>
2023-07-23 18:04:54 +0200

    small details

--- Types.js
+++ Types.js
@@ -4,3 +4,11 @@ console.log(typeof js);
 
 console.log(typeof "Nick");
 console.log(typeof 36);
+
+let year;
+console.log(typeof year);
+
+year = 11;
+console.log(typeof year);
+console.log(typeof null);
+
