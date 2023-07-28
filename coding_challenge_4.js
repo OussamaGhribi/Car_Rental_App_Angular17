795cc236bbb8c8308c7e4eb9215608d6d7be39fe
Oussamaghribi <ghribioussama98@gmail.com>
2023-07-28 16:32:49 +0200

    finishing the challenge

--- coding_challenge_4.js
+++ coding_challenge_4.js
@@ -3,3 +3,12 @@ const data2 = [12 , 5 , -5 , 0 , 4];
 
 console.log(`... ${data1[0]}C ... ${data1[1]}C ... ${data1[2]}C`);
 
+const printForecast = function (arr) {
+    let str = '';
+    for (let i = 0 ; i < arr.length ; i++){
+        str += `${arr[i]}C in ${i + 1} days ...`;
+    }
+    console.log('...' + str);
+};
+
+printForecast(data1);
