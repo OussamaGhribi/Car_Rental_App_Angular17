13afe9f902d8bf1c15250a5ffa74b1123d610fc3
Oussamaghribi <ghribioussama98@gmail.com>
2023-07-31 16:39:49 +0200

    code project

--- Selector.js
+++ Selector.js
@@ -7,3 +7,23 @@ document.querySelector('.scrore').textContent = 10;
 
 document.querySelector('.guess').value = 23;
 console.log(document.querySelector('.guess').value);
+
+/*=======================================================================*/
+
+document.querySelector('.check').addEventListener('click',function(){
+    const guess = Number(document.querySelector('.guess').value);
+    console.log(guess , typeof guess);
+    const secretNumber = Math.trunc(Math.random()*20) + 1;
+
+    if (!guess) {
+        document.querySelector('.message').textContent = 'No Number';
+    }else if (guess === secretNumber) {
+        document.querySelector('.message').textContent = 'Correct Number';
+    }else if (guess > secretNumber) {
+        document.querySelector('.message').textContent = 'Number too high';
+        score--;
+    }else if (guess < secretNumber) {
+        document.querySelector('.message').textContent = 'Number too low';
+        score--;
+    }
+})
---
+++ coding_project_1.js
@@ -0,0 +1,17 @@
+document.querySelector('.check').addEventListener('click',function(){
+    const guess = Number(document.querySelector('.guess').value);
+    console.log(guess , typeof guess);
+    const secretNumber = Math.trunc(Math.random()*20) + 1;
+
+    if (!guess) {
+        document.querySelector('.message').textContent = 'No Number';
+    }else if (guess === secretNumber) {
+        document.querySelector('.message').textContent = 'Correct Number';
+    }else if (guess > secretNumber) {
+        document.querySelector('.message').textContent = 'Number too high';
+        score--;
+    }else if (guess < secretNumber) {
+        document.querySelector('.message').textContent = 'Number too low';
+        score--;
+    }
+})
