9222fcfce2d07ec9968674b776eebac5ea930142
Oussamaghribi <ghribioussama98@gmail.com>
2023-07-31 10:41:08 +0200

    query selector

---
+++ Selector.js
@@ -0,0 +1,9 @@
+
+console.log(document.querySelector('.message').textContent);
+document.querySelector('.message').textContent = 'Correct number !';
+
+document.querySelector('.number').textContent = 13;
+document.querySelector('.scrore').textContent = 10;
+
+document.querySelector('.guess').value = 23;
+console.log(document.querySelector('.guess').value);
