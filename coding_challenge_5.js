a13bb5f5351c5f3956ba5e68f6a4f99a0095fb39
Oussamaghribi <ghribioussama98@gmail.com>
2023-08-06 12:03:31 +0200

    close to finish

--- coding_challenge_5.js
+++ coding_challenge_5.js
@@ -17,3 +17,27 @@ let activePlayer = 0;
 score0EL.textContent = 0;
 score1EL.textContent = 0;
 diceEL.classList.add("hidden");
+
+btnRoll.addEventListener('click' , function() {
+
+    const dice = Math.trunc(Math.random()*6)+1;
+
+    diceEL.classList.remove('hidden');
+    diceEL.src = `dice-${dice}.png`; //cause in html there is [ <img src="dice-0.png">] we change the number 0
+
+    if (dice = 1){
+        currentScore += dice;
+        document.getElementById(`current--${activePlayer}`).textContent = currentScore;
+    }else {
+        document.getElementById(`current--${activePlayer}`).textContent = 0;
+        currentScore = 0;
+        if (activePlayer === 0) {
+            activePlayer = 1;
+        }else {
+            activePlayer = 0;
+        }
+        player0EL.classList.toggle("active-player");
+        player1EL.classList.toggle("active-player");
+    }
+})
+
